0d84fbcf4af29326938d95c5c871a1c1aee4717b
Bo Anderson <mail@boanderson.me>
2019-11-22 21:29:50 +0000

    b: no longer depends on a

--- Formula/b.rb
+++ Formula/b.rb
@@ -1,10 +1,9 @@
 class B < Formula
   url "https://www.w3.org/TR/PNG/iso_8859-1.txt"
   sha256 "3aff1954277c4fc27603346901e4848b58fe3c8bed63affe6086003dd6c2b9fe"
+  revision 1
 
   :keg_only
-
-  depends_on "bo98/experimental/a"
 
   def install
     (buildpath/"test.c").write <<~EOS
@@ -14,7 +13,6 @@ class B < Formula
       }
     EOS
     system "clang", "-dynamiclib",
-                    "-L#{Formula["bo98/experimental/a"].opt_lib}", "-lA",
                     "-olibB.dylib",
                     "test.c"
     lib.install "libB.dylib"
